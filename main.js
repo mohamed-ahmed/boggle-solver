fe8bfcaf237aeac975a30fa5c829db82739cb4f4
Mohamed Ahmed <mohamed.ahmed@live.ca>
2015-02-15 18:58:52 -0500

    bogger solver now functional

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 fs = require('fs');
 var trie = new Trie();
 
+var resultsList = [];
+
 fs.readFile('dictionary.txt', 'utf8', function (err, data){
 	if(err){
 		console.log(err);
@@ -10,20 +12,71 @@ fs.readFile('dictionary.txt', 'utf8', function (err, data){
 	var wordArray = [];
 	for(var i in lineArray){
 		if(lineArray[i].length > 0)
-			var word = lineArray[i].split(' ')[0];
-			if(word.length > 2){
-				wordArray.push(word);
-				trie.add(word);
-			}
+			var word = lineArray[i].split(' ')[0].toLowerCase();
+		if(word.length > 2){
+			//wordArray.push();
+			trie.add(word);
+		}
 	}
 	console.log(wordArray.length);
 	console.log(trie.subKeys);
+	console.log(trie.get('apex'));
+	console.log(trie.get('ape'));
+	console.log(trie.get('ap'));
+
+	solve();
+
+	console.log("resultsList");
+	console.log(resultsList);
+
 });
 
+var puzzle = [
+['f','x','i','e'],
+['a','m','i','o'],
+['e','w','l','x'],
+['a','s','b','u']
+];
 
 
+function solve(){
+	for(var r = 0; r < puzzle.length ; r++){
+		for(var c = 0 ; c < puzzle[0].length ; c++){
+			getWords(r,c, puzzle[r][c]);
+			
+		}
+	}
+}
 
+function inBoundary(r,c){
+	if(r >= 0 && r < puzzle.length && c >= 0 && c < puzzle[0].length){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
 
+function getWords(r, c, string){
+	var offsetArray = [-1,0,1];
+	console.log('getWords ' + string);
+	if(trie.get(string) == 'word'){
+		resultsList.push(string)
+	}
+	if(trie.get(string) != 'prefix' && trie.get(string) != 'word' ){
+		return;
+	}
+	for( var rOffset in offsetArray){
+		var rIndex = r + offsetArray[rOffset];
+		for(var cOffset in offsetArray){
+			var cIndex = c + offsetArray[cOffset];
+			if( !(rIndex == 0 && cIndex == 0) && inBoundary(rIndex, cIndex)){
+				console.log("indexes: " + rIndex + "," + cIndex);
+				getWords(rIndex, cIndex, string + puzzle[rIndex][cIndex]);
+			}
+		}
+	}
+}
 
 
 
@@ -63,9 +116,10 @@ fs.readFile('dictionary.txt', 'utf8', function (err, data){
 		this.subKeys[string[0]].key = string[0];
 		if(remainderString.length > 0){
 			this.subKeys[string[0]].add(remainderString);
+			this.subKeys[string[0]].value = 'prefix';
 		}
 		else{
-			this.subKeys[string[0]].value = true;
+			this.subKeys[string[0]].value = 'word';
 		}
 	}
 
